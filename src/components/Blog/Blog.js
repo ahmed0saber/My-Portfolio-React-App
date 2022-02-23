11e8e0142aa0b05bd12a24f23dfc0ff6ba185f76
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-24 01:49:04 +0200

    Add files via upload

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,12 +1,98 @@
 import React, { Component } from "react";
+import { Outlet, NavLink } from "react-router-dom";
 import "./Blog.css"
 
 class Blog extends Component {
     render(){
         return (
-            <main className="blog">
-                <h1>Blog</h1>
-                <p>Here I talk about my interests</p>
+            <main className="projects">
+                <h1>My Blog</h1>
+                <section className="projects-container">
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog1.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog3.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog2.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog3.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog2.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/blog1.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>My Goals in 2022</h3>
+                                <p>Here, I'm talk about lorem, lorem and lorem</p>
+                                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                <NavLink to="/">Read More</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+                </section>
             </main>
         )
     }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,97 @@
 import React, { Component } from "react";
+import { Outlet, NavLink } from "react-router-dom";
 import "./Projects.css"
 
 class Projects extends Component {
     render(){
         return (
             <main className="projects">
-                <h1>Projects</h1>
-                <p>A place where you can view most of my projects</p>
+                <h1>My Projects</h1>
+                <section className="projects-container">
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+                    
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+
+                    <div className="project">
+                        <span className="fa fa-arrow-right"></span>
+                        <img src="./images/projects/xo.png" alt="XO Online"/>
+                        <div className="project-back">
+                            <div>
+                                <h3>XO Online</h3>
+                                <p>An online game</p>
+                                <NavLink to="/">View a demo</NavLink>
+                                <NavLink to="/">View on GitHub</NavLink>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+                </section>
             </main>
         )
     }
