90d0fb604097cf61f1d3c4ce261050f41a7cb3a5
Ahmed Saber <ahmed0saber33@gmail.com>
2023-06-01 03:53:54 +0300

    Update facebook link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
                             <p>Frontend Web Developer</p>
                         </section>
                         <section className="social-media-icons">
-                            <a aria-label="Facebook" style={{"--color": "#00e"}} rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100004875915808"><i className="fa fa-facebook"></i></a>
+                            <a aria-label="Facebook" style={{"--color": "#00e"}} rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100092736532709"><i className="fa fa-facebook"></i></a>
                             <a aria-label="WhatsApp" style={{"--color": "#0c0"}} rel="noreferrer" target="_blank" href="https://api.whatsapp.com/send?phone=+201208611892&text=Hi"><i className="fa fa-whatsapp"></i></a>
                             <a aria-label="LinkedIn" style={{"--color": "#0bb"}} rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/ahmed0saber/"><i className="fa fa-linkedin"></i></a>
                             <a aria-label="GitHub" style={{"--color": "#b0b"}} rel="noreferrer" target="_blank" href="https://github.com/ahmed0saber"><i className="fa fa-github"></i></a>
